9a636c5d4b2c7c3e1aed00fe6763e829def12d7c
Raí Joia <megac154@gmail.com>
2024-01-05 12:11:47 -0300

    memo(type): tipando os pensamentos

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,29 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Angular 2',
-      autoria: 'Raí',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo: 'React',
-      autoria: 'Raí',
-      modelo: 'modelo1',
-    },
-    {
-      conteudo: 'Next',
-      autoria: 'Raí',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quisquam tenetur voluptas, inventore fuga cumque eveniet maiores consequatur molestiae voluptates sed possimus non dolores expedita temporibus praesentium! Impedit, earum ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quisquam tenetur voluptas, inventore fuga cumque eveniet maiores consequatur molestiae voluptates sed possimus non dolores expedita temporibus praesentium! Impedit, earum ullam.',
-      autoria: 'Raí',
-      modelo: 'modelo1',
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() {}
 
