0e2fea62efec9fbf39787a5bd4bdbfd9ceb991ee
Raí Joia <megac154@gmail.com>
2024-01-05 11:36:50 -0300

    memo(component): component pensamento criado e aplicado

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+      conteudo: 'Angular 2',
+      autoria: 'Raí',
+      modelo: 'modelo2',
+    },
+    {
+      conteudo: 'React',
+      autoria: 'Raí',
+      modelo: 'modelo1',
+    },
+    {
+      conteudo: 'Next',
+      autoria: 'Raí',
+      modelo: 'modelo3',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
