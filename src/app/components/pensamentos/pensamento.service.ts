0e1d08020dc8bcdc28050680c667470ed0bcc1f4
Raí Joia <megac154@gmail.com>
2024-01-05 16:04:30 -0300

    memo(service): method get ok

--- src/app/components/pensamentos/pensamento.service.ts
+++ src/app/components/pensamentos/pensamento.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import type { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PensamentoService {
+  private readonly API = 'http://localhost:3000/pensamentos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
 }
